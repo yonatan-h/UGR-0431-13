2037db4086e8e20acdc4c04eef9105ab08381f05
yonatan-h <yon019hl@gmail.com>
2023-01-17 17:17:26 +0300

    used a redirector page so that github pages works

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector(".main-header__menu-button");
-const menu = document.querySelector(".main-header__nav");
-
-menuButton.addEventListener("click", () => {
-  menu.classList.toggle("main-header__nav--hide");
-});
--- js/services.js
+++
@@ -1,9 +0,0 @@
-const transcripts = [...document.querySelectorAll(".transcript")];
-transcripts.forEach((transcript) => {
-  const toggler = transcript.querySelector(".transcript__toggle");
-  const content = transcript.querySelector(".transcript__lines-wrapper");
-
-  toggler.addEventListener("click", () => {
-    content.classList.toggle("transcript__lines-wrapper--hidden");
-  });
-});
